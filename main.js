685ca319ea378e947d982702d964ce8154f197a4
Kamil Naja <kamilnaja@gmail.com>
2017-04-22 22:30:36 +0200

    wip

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function ($scope, $http) {
     $http.get('users.json')
-        .then(function(response) {
+        .then(function (response) {
             $scope.myUsers = response.data;
-            console.log($scope.myUsers.people[1].name);
-        })
+        });
+
+    $scope.selectUser = function (user) {
+        console.log(user);
+        $scope.clickedUser = user;
+    };
+    $scope.editItem = function () {
+        console.log("edit");
+    };
+    $scope.deleteItem = function (name) {
+        $scope.users.splice($scope.users.indexOf($scope.clickedUser))
+    };
 });
+
+//todo add checkbox
